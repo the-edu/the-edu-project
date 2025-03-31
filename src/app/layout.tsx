ee035d3ae401bcdaa5f76dc2b48409491b7e2eae
신상호 <94950829+nononcrust@users.noreply.github.com>
2025-03-31 15:41:11 +0900

    chore: 프로젝트 설정 수정 (#3)
    * tsconfig noUncheckedIndexedAccess 옵션 추가
    
    * endOfLine auto -> lf
    
    * check-types:watch 스크립트 추가
    
    * styles 파일 위치 변경, tailwindcss 기본 스타일 제거
    
    * nextjs 기본 파일 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 import { GlobalProvider } from '@/providers/global-provider';
-
-import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ko">
+      <body className="antialiased">
         <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
